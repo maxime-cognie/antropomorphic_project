7381595250f27d1af6b96467813975b835e0698c
maxime.cognie <maxime.cognie@protonmail.com>
2024-07-31 06:49:56 +0000

    encapsulate the matrix generation scripts in a class

--- scripts/generate_matrixes.py
+++ scripts/generate_matrixes.py
@@ -1,57 +1,94 @@
 #!/usr/bin/env python3
 
-from sympy import Matrix, cos, sin, Symbol, simplify, trigsimp, pi, preview
+from sympy import Matrix, cos, sin, Symbol, trigsimp, pi, preview
 from sympy.interactive import printing
 
 
-# To make display prety
-printing.init_printing(use_latex = True)
+class DHParameters():
+    def __init__(self, DH_param):
+        # DH parameters
+        self.DH_param_ = DH_param
+        self.theta = self.get_dh_param("theta")
+        self.alpha = self.get_dh_param("alpha")
+        self.d = self.get_dh_param("d")
+        self.r = self.get_dh_param("r")
 
-theta_i = Symbol("theta_i")
-alpha_i = Symbol("alpha_i")
-r_i = Symbol("r_i")
-d_i = Symbol("d_i")
+    def get_dh_param(self, name):
 
-DH_Matric_Generic = Matrix([[cos(theta_i), -sin(theta_i)*cos(alpha_i), sin(theta_i)*sin(alpha_i), r_i*cos(theta_i)],
-                            [sin(theta_i), cos(theta_i)*cos(alpha_i), -cos(theta_i)*sin(alpha_i), r_i*sin(theta_i)],
-                            [0, sin(alpha_i), cos(alpha_i), d_i],
+        if name in self.DH_param_:
+            return self.DH_param_[name]
+        else:
+            assert False, "Asked for Non existen param DH name ="+str(name)
+    
+    def __str__(self):
+        return f'DH parameters:\n theta: {self.theta}\n alpha: {self.alpha}\n d: {self.d}\n r: {self.r}'
+        
+
+class HomogeneousMatrix():
+    def __init__(self):
+        self.A = Matrix([
+            [1,0,0,0],
+            [0,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1]])
+    
+    def __mul__(self, other):
+        return self.A * other.A
+
+    def compute_matrix(self, dh_param):
+        print("Compute Matrix for " + str(dh_param))
+        self.A = Matrix([[cos(dh_param.theta), -sin(dh_param.theta)*cos(dh_param.alpha), sin(dh_param.theta)*sin(dh_param.alpha), dh_param.r*cos(dh_param.theta)],
+                            [sin(dh_param.theta), cos(dh_param.theta)*cos(dh_param.alpha), -cos(dh_param.theta)*sin(dh_param.alpha), dh_param.r*sin(dh_param.theta)],
+                            [0, sin(dh_param.alpha), cos(dh_param.alpha), dh_param.d],
                             [0,0,0,1]])
+    
+    def generate_matrix(self, file_name):
+        printing.init_printing(use_latex = True)
+        print("Generate Matrix: " + file_name)
+        preview(self.A, viewer="file", filename=file_name+".png", dvioptions=["-D","300"])
+    
+    def simplify(self):
+        self.A = trigsimp(self.A)
 
-# Now create A01, A12, A23
+def main():
+    DH_param_1 = DHParameters(
+        {"theta":Symbol("theta_1"),
+        "alpha":pi/2,
+        "d":0,
+        "r":0})
+    
+    DH_param_2 = DHParameters(
+        {"theta":Symbol("theta_2"),
+        "alpha":0,
+        "d":0,
+        "r":Symbol("r_2")})
+    
+    DH_param_3 = DHParameters(
+        {"theta":Symbol("theta_3"),
+        "alpha":0,
+        "d":0,
+        "r":Symbol("r_3")})
+    
+    A_0_1 = HomogeneousMatrix()
+    A_1_2 = HomogeneousMatrix()
+    A_2_3 = HomogeneousMatrix()
 
-theta_1 = Symbol("theta_1")
-theta_2 = Symbol("theta_2")
-theta_3 = Symbol("theta_3")
+    A_0_1.compute_matrix(DH_param_1)
+    A_1_2.compute_matrix(DH_param_2)
+    A_2_3.compute_matrix(DH_param_3)
 
+    A_0_1.generate_matrix("A_0_1")    
+    A_1_2.generate_matrix("A_1_2")    
+    A_2_3.generate_matrix("A_2_3")
 
-alpha_planar = 0.0
+    A_0_2 = HomogeneousMatrix()
+    A_0_3 = HomogeneousMatrix() 
 
-alpha_1 = pi/2
-alpha_2 = alpha_planar
-alpha_3 = alpha_planar
-
-r_1 = 0.0
-r_2 = Symbol("r_2")
-r_3 = Symbol("r_3")
-
-d_planar = 0.0
-d_1 = d_planar
-d_2 = d_planar
-d_3 = d_planar
-
-A01 = DH_Matric_Generic.subs(r_i,r_1).subs(alpha_i,alpha_1).subs(d_i,d_1).subs(theta_i, theta_1)
-A12 = DH_Matric_Generic.subs(r_i,r_2).subs(alpha_i,alpha_2).subs(d_i,d_2).subs(theta_i, theta_2)
-A23 = DH_Matric_Generic.subs(r_i,r_3).subs(alpha_i,alpha_3).subs(d_i,d_3).subs(theta_i, theta_3)
-
-A03 = A01 * A12 * A23
-
-A03_simplify = trigsimp(A03)
-
-# We save
-
-preview(A03, viewer='file', filename="A03.png", dvioptions=['-D','300'])
-
-preview(A01, viewer='file', filename="A01.png", dvioptions=['-D','300'])
-preview(A12, viewer='file', filename="A12.png", dvioptions=['-D','300'])
-preview(A23, viewer='file', filename="A23.png", dvioptions=['-D','300'])
-preview(A03_simplify, viewer='file', filename="A03_simplify.png", dvioptions=['-D','300'])
+    A_0_2.A = A_0_1 * A_1_2
+    A_0_3.A = A_0_2 * A_2_3
+    A_0_3.generate_matrix("A_0_3")
+    A_0_3.simplify()
+    A_0_3.generate_matrix("A_0_3_simplified")
+    
+if __name__ == "__main__":
+    main()
